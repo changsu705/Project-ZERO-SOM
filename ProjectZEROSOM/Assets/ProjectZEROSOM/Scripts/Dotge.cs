28751d242a30348679dea08374abf15839d1a56a
changsu705 <2021137031@g.shingu.ac.kr>
2025-02-28 02:17:45 +0900

    feat : 폴더 이름 정리 그리고 닷지 코드 임시 작성,
    제곧내

--- ProjectZEROSOM/Assets/ProjectZEROSOM/Scripts/Dotge.cs
+++ ProjectZEROSOM/Assets/ProjectZEROSOM/Scripts/Dotge.cs
@@ -7,11 +7,19 @@ using UnityEngine.XR;
 public class Dotge : MonoBehaviour
 {
     private InputDevice hmdDevice;
+    private Rigidbody rb;
+    public float DotgePower = 10f; 
 
     void Start()
     {
-        // HMD 장치를 찾습니다. (컨트롤러나 다른 디바이스의 자이로 데이터를 원한다면 해당 특성으로 필터링 가능)
+        rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("리지드바디 검출 안됌.");
+            return;
+        }
         var devices = new List<InputDevice>();
+        // HMD 장치를 찾습니다. (컨트롤러나 다른 디바이스의 자이로 데이터를 원한다면 해당 특성으로 필터링 가능)
         InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeadMounted, devices);
         // GetDevicesWithCharacteristics 장치를 찾습니다.(InputDeviceCharacteristics.HeadMounted 인풋 디바이스 사용장치중에 해드 마운티드(헤드셋)을
         if (devices.Count > 0)
@@ -29,8 +37,19 @@ public class Dotge : MonoBehaviour
             // hmdDevice.TryGetFeatureValue hmdDevice에서 값을 받아오는것을 시도합니다. CommonUsages.deviceAngularVelocity 장치의 각속도를 out Vector3 angularVelocity 그것을 여기에 저장합니다. 해당 TryGetFeatureValue 문은 true false를 반환하기때문에 성공 여부를 파악 할 수 있다.
             {
                 Debug.Log("Angular Velocity: " + angularVelocity);
-                //이후 여기에 벡터 방향 검출후 그 방향으로 포스 추가하는 구문 넣어야함.
+                DotgeImpulse(angularVelocity);
             }
         }
     }
+    void DotgeImpulse(Vector3 angularVelocity)
+    {
+        if (rb == null) return;
+
+        Vector3 dotgeDirection = angularVelocity.normalized; // 방향 벡터 정규화
+        rb.AddForce(dotgeDirection * DotgePower, ForceMode.Impulse); // 순간적인 힘 추가
+
+        //현제는 정규환된후 그 방향으로 힘을 추가하는 코드밖에 안들어가 있음
+        //y축 방향을 제거한뒤 정규화 그리고 해당 값을 넘겼을때 일정한 백터를 추가해야함.
+    }
+
 }
