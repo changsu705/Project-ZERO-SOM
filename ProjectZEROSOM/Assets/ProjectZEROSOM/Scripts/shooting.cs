1c6e94970a52f70b7c02d8600487f31f6dcc8e7b
changsu705 <20211137031@g.shingu.ac.kr>
2025-03-18 12:17:59 +0900

    [fix] 수업시간에 한 것
    슈팅 변경

--- ProjectZEROSOM/Assets/ProjectZEROSOM/Scripts/shooting.cs
+++ ProjectZEROSOM/Assets/ProjectZEROSOM/Scripts/shooting.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class shooting : MonoBehaviour
 {
-        public int damage = 10; // 투사체 공격력
+    public int damage = 10; // 투사체 공격력
+    public Transform firePoint;
+    public float projectileSpeed = 10f;
 
-        private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        Unit unit = other.GetComponent<Unit>();
+        if (unit != null) // Unit 클래스를 상속받은 오브젝트인지 확인
         {
-            Unit unit = other.GetComponent<Unit>();
-            if (unit != null) // Unit 클래스를 상속받은 오브젝트인지 확인
-            {
-                unit.TakeDamage(damage);
-                Destroy(gameObject); // 투사체 제거
-            }
+            unit.TakeDamage(damage);
+            Destroy(gameObject); // 투사체 제거
         }
+    }
 }
